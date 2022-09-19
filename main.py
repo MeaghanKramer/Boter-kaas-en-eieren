c0df45d1dd653910c75827fb2cfb4211b5491f1f
Meaghan Kramer <74553139+MeaghanKramer@users.noreply.github.com>
2022-09-19 14:09:46 +0000

    2 (slimme) agenten spelen tegen elkaar

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
-import random
-from bke import EvaluationAgent, start
+
+from bke import EvaluationAgent, start, can_win
 
 class MyRandomAgent(EvaluationAgent):
-  def evaluate (self, board, y_symbol, opponent_symbol):
-    return random.randint(1, 500)
+  def evaluate (self, board, my_symbol, opponent_symbol):
+    getal = 1
+    if can_win(board, opponent_symbol):
+      getal = getal - 1000
+    return getal
+
 
 my_random_agent = MyRandomAgent()
 start(player_o=my_random_agent, player_x=my_random_agent)
