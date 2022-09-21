96d88457238e4e3dadb0fec9e310f2250a86f3fe
Meaghan Kramer <74553139+MeaghanKramer@users.noreply.github.com>
2022-09-21 08:05:24 +0000

    Begin menu gestart + def plot gemaakt

--- main.py
+++ main.py
@@ -14,16 +14,42 @@ class MyAgent(MLAgent):
         return reward
     
     
+
 random.seed(1)
 
-my_agent = MyAgent(alpha=0.8, epsilon=0.2)
+def plot1():
+def plot2():
+def plot3():
+def plot4():
+def plot5():
+  my_agent = MyAgent(alpha=0.8, epsilon=0.2)
+  
+  my_agent = MyAgent()
+  random_agent = RandomAgent()
+   
+  train_and_plot(
+      agent=my_agent,
+      validation_agent=random_agent,
+      iterations=20,
+      trainings=50,
+      validations=1000)
 
-my_agent = MyAgent()
-random_agent = RandomAgent()
- 
-train_and_plot(
-    agent=my_agent,
-    validation_agent=random_agent,
-    iterations=50,
-    trainings=100,
-    validations=1000)
+
+print("1. speel tegen een medespeler")
+print("2. speel tegen een beginner")
+print("3. train je tegenstander")
+print("4. speel tegen een expert")
+print("5. train, valideer en plot")
+
+i = input()
+if i == "1":
+  plot1()
+elif i == "2":
+  plot2()
+elif i == "3":
+  plot3()
+elif i == "4":
+  plot4()
+elif i == "5":
+  plot5()
+  
